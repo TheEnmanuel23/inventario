2e50475b0fbe7df005b9a5cc6dcf5d4cd30f9d3d
Enmanuel Jarquin <enmisac@gmail.com>
2020-09-05 12:07:31 -0600

    set scripts

---
+++ build/main.js
@@ -0,0 +1,221 @@
+require('source-map-support/register');
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/importResolvers.js":
+/*!********************************!*\
+  !*** ./src/importResolvers.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(__dirname) {const path = __webpack_require__(/*! path */ "path");
+
+const {
+  mergeResolvers
+} = __webpack_require__(/*! @graphql-tools/merge */ "@graphql-tools/merge");
+
+const {
+  loadFilesSync
+} = __webpack_require__(/*! @graphql-tools/load-files */ "@graphql-tools/load-files");
+
+const resolversArray = loadFilesSync(path.join(__dirname, "./**/resolvers.js"));
+module.exports = mergeResolvers(resolversArray);
+/* WEBPACK VAR INJECTION */}.call(this, "src"))
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+const {
+  ApolloServer
+} = __webpack_require__(/*! apollo-server */ "apollo-server");
+
+const typeDefs = __webpack_require__(/*! ./typeDefs */ "./src/typeDefs.js");
+
+const resolvers = __webpack_require__(/*! ./importResolvers */ "./src/importResolvers.js");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+server.listen().then(res => {
+  console.log(`🚀 Server ready at ${res.url}`);
+});
+
+/***/ }),
+
+/***/ "./src/typeDefs.js":
+/*!*************************!*\
+  !*** ./src/typeDefs.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(__dirname) {const path = __webpack_require__(/*! path */ "path");
+
+const {
+  mergeTypeDefs
+} = __webpack_require__(/*! @graphql-tools/merge */ "@graphql-tools/merge");
+
+const {
+  loadFilesSync
+} = __webpack_require__(/*! @graphql-tools/load-files */ "@graphql-tools/load-files");
+
+const typesArray = loadFilesSync(path.join(__dirname, "./**/*.graphql"));
+module.exports = mergeTypeDefs(typesArray, {
+  all: true
+});
+/* WEBPACK VAR INJECTION */}.call(this, "src"))
+
+/***/ }),
+
+/***/ 0:
+/*!****************************!*\
+  !*** multi ./src/index.js ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/enmanueljarquin/Development/courses/appGraphql/inventario/src/index.js */"./src/index.js");
+
+
+/***/ }),
+
+/***/ "@graphql-tools/load-files":
+/*!********************************************!*\
+  !*** external "@graphql-tools/load-files" ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("@graphql-tools/load-files");
+
+/***/ }),
+
+/***/ "@graphql-tools/merge":
+/*!***************************************!*\
+  !*** external "@graphql-tools/merge" ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("@graphql-tools/merge");
+
+/***/ }),
+
+/***/ "apollo-server":
+/*!********************************!*\
+  !*** external "apollo-server" ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("apollo-server");
+
+/***/ }),
+
+/***/ "path":
+/*!***********************!*\
+  !*** external "path" ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("path");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=main.map
